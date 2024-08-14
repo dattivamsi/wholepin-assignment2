4ce009ec9acd67e3902f6a629892f0833049474a
dattivamsi <dattitarun@gmail.com>
2024-08-14 22:55:55 +0530

    minor changes

--- src/components/ArtifactCard.jsx
+++ src/components/ArtifactCard.jsx
@@ -1,32 +1,39 @@
 import React, { useEffect, useState } from "react";
 import "./ArtifactCard.css";
 
-const ArtifactCard = ({ artifact,gridContainer }) => {
-    const [description,setDecription] = useState("")
-     const backgroundColor = artifact?.color || '#fff'
+const ArtifactCard = ({ artifact, gridContainer }) => {
+  const [description, setDecription] = useState("");
+  const backgroundColor = artifact?.color || "#fff";
 
-    useEffect(()=>{
-        const descLen = 100;
-        const descdata = artifact?.description?.length > descLen ? artifact?.description?.slice(0,descLen) : artifact?.description 
-        setDecription(descdata)
-    },[])
-    
-    
+  useEffect(() => {
+    const descLen = 100;
+    const descdata =
+      artifact?.description?.length > descLen
+        ? artifact?.description?.slice(0, descLen)
+        : artifact?.description;
+    setDecription(descdata);
+  }, []);
+
   return (
-    <div className={ gridContainer === "grid" ? "artifact-card" :"artifact-flat-card"}>
-      <img src={artifact?.imageUrl} alt={artifact?.title} 
-    //   style={{ backgroundColor: backgroundColor }}
+    <div
+      className={
+        gridContainer === "grid" ? "artifact-card" : "artifact-flat-card"
+      }
+    >
+      <img
+        src={artifact?.imageUrl}
+        alt={artifact?.title}
+        //   style={{ backgroundColor: backgroundColor }}
       />
-      <div>
+      <div className={gridContainer === "grid" ? "response-grid":"response-card"}>
+        <div className="artifact-title">
+          <h3>{artifact.title}</h3>
+          <p className="period">{artifact.period}</p>
+        </div>
 
-      <div className="artifact-title">
-        <h3>{artifact.title}</h3>
-        <p className="period">{artifact.period}</p>
+        <p className="description">{description}</p>
       </div>
-      
-      <p className="description">{description}</p>
-      </div>
-      <div></div>
+      <div className="dummy"></div>
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,9 @@ const Header = ({setSearchTerm,setShowFilter,showFilter}) => {
   return (
     <div className="header_container">
       <div className="header">
+        {/* <label htmlFor="search" style={{display:"block"}}>Search</label> */}
         <input
+        id="search"
           type="text"
           placeholder="Search from the worlds antiquue collection"
           onChange={(e)=>handleChange(e)}
